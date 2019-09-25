89b36ac682925dbc8adcd5db805dd27ef363be3d
807715333@qq.com <807715333@qq.com>
2019-09-25 15:47:34 +0800

    12

--- c++/ANN-master/ANN-master/src/com/jingchen/util/DataUtil.java
+++ c++/ANN-master/ANN-master/src/com/jingchen/util/DataUtil.java
@@ -3,7 +3,7 @@ package com.jingchen.util;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
+import java.util.;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
